36ef7272a6488c611c7ed306b38bfb6d340b97aa
anupambhattacharya1 <108210612+anupambhattacharya1@users.noreply.github.com>
2025-08-01 11:51:26 +0530

    Add files via upload

---
+++ Python/Activities/test_Activity1.py
@@ -0,0 +1,14 @@
+	
+import pytest
+import math
+ 
+def test_sqrt():
+   num = 25
+   assert math.sqrt(num) == 5
+ 
+def test_square():
+   num = 7
+   assert num*num == 49
+ 
+def test_quality():
+   assert 10 == 10
